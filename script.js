671e9a59edab96e5e151c8b37042ad25727c02f6
gehadsamy <gehadsamy96@gmail.com>
2021-11-11 13:11:19 +0200

    bootsrtap project

---
+++ script.js
@@ -0,0 +1,24 @@
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    padding: '5rem',
+  } );
+  
+  splide.mount();
+
+  const btnLang = document.getElementById("lang");
+const html = document.getElementById("html");
+const head = document.getElementById("head");
+const body = document.getElementById("body");
+
+
+btnLang.addEventListener("click" , ()=>{
+    if(html.hasAttribute("dir")){
+        html.removeAttribute("dir")
+        body.classList.remove("rtl")
+        btnLang.innerText = "AR";
+    }else{
+        html.setAttribute("dir" , "rtl")
+        body.classList.add("rtl")
+        btnLang.innerText = "EN";
+    }
+})
